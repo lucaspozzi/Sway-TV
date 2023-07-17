2badd61d3346402e966b8ad3dd11d22745826d0f
luc <lucaspozzi1@outlook.com>
2023-07-17 17:33:02 -0400

    what's happening?

---
+++ Sway Radio/Sway_RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sway_RadioApp.swift
+//  Sway Radio
+//
+//  Created by Lucas Pozzi de Souza on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Sway_RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
