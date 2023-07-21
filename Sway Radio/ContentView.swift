23be1f15ad0328d01d3613828142127302bcbda7
luc <lucaspozzi1@outlook.com>
2023-07-21 01:15:20 -0400

    LP: home page feature flags

--- Sway Radio/ContentView.swift
+++ Sway Radio/ContentView.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import CloudKit
 
 struct ContentView: View {
-    @StateObject var audioPlayer = AudioPlayer()
     
+    @StateObject var audioPlayer = AudioPlayer()
+    @State private var isEventsTabEnabled = false
+    private let container = CKContainer(identifier: "iCloud.app.waggie.Sway-TV")
+    private let publicDatabase = CKContainer(identifier: "iCloud.app.waggie.Sway-TV").publicCloudDatabase
+    private let recordType = "FeatureFlags"
+
+    func fetchFeatureFlag(named featureName: String) {
+        let predicate = NSPredicate(format: "featureName = %@", featureName)
+        let query = CKQuery(recordType: recordType, predicate: predicate)
+        
+        publicDatabase.fetch(withQuery: query, inZoneWith: nil, desiredKeys: nil, resultsLimit: 1) { result in
+            switch result {
+            case .failure(let error):
+                print("An error occurred: \(error.localizedDescription)")
+            case .success((let matchResults, _)):
+                if let record = matchResults.first?.1,
+                   case let .success(recordData) = record,
+                   let isEnabled = recordData["isEnabled"] as? Int64 {
+                    DispatchQueue.main.async {
+                        self.isEventsTabEnabled = isEnabled != 0
+                    }
+                }
+            }
+        }
+    }
+
+
     var body: some View {
         TabView {
             
@@ -32,14 +59,20 @@ struct ContentView: View {
                 Text("Recent")
             }
             
-            NavigationView {
-                EventScheduleView()
-                    .navigationTitle("Event Schedule")
+            if(isEventsTabEnabled){
+                NavigationView {
+                    EventScheduleView()
+                        .navigationTitle("Event Schedule")
+                }
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("Events")
+                }
             }
-            .tabItem {
-                Image(systemName: "calendar")
-                Text("Events")
-            }
+            
+        }
+        .onAppear {
+            fetchFeatureFlag(named: "isEventsTabEnabled")
         }
     }
 }
