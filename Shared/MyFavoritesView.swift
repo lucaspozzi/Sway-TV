74ccb7d37a5d5f78e723a41505ee727e98205418
luc <lucaspozzi1@outlook.com>
2023-08-12 01:23:29 -0400

    LP: add some animation

--- Shared/MyFavoritesView.swift
+++ Shared/MyFavoritesView.swift
@@ -66,6 +66,7 @@ struct MyFavoritesView: View {
                                 .frame(width: 60, height: 60)
                                 .cornerRadius(8) // Apply corner radius for image
                                 .padding(.trailing, 10)
+                                .animation(.default)
                         case .failure:
                             defaultImage
                                 .resizable()
