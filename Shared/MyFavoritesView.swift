3920114fde8a7bc9a50900681e98ed61664eb1ab
luc <lucaspozzi1@outlook.com>
2023-08-11 23:31:34 -0400

    LP: pull down to refresh my favorites

--- Shared/MyFavoritesView.swift
+++ Shared/MyFavoritesView.swift
@@ -13,6 +13,31 @@ struct MyFavoritesView: View {
     @State private var listItems: [String] = []
     private let sentiments = Sentiments()
     
+    func loadData() {
+        var newList: [String] = []
+        sentiments.fetchUserFavorites { (tracks, error) in
+            if let error = error {
+                print("Error fetching top tracks: \(error)")
+            } else if let tracks = tracks {
+                for record in tracks {
+                    if let currentTrack = record["currentTrack"] as? String {
+                        newList.append(currentTrack)
+                    }
+                }
+                listItems = newList
+            }
+        }
+    }
+    
+    func refreshData() async {
+        do {
+            try await Task.sleep(nanoseconds: 1_000_000_000) // Simulating a refresh delay
+            loadData()
+        } catch {
+            print("Error during refreshData: \(error)")
+        }
+    }
+    
     var body: some View {
         List(listItems, id: \.self) { track in
             HStack(alignment: .top) {
@@ -20,20 +45,11 @@ struct MyFavoritesView: View {
             }
             .padding()
         }
+        .refreshable {
+            await refreshData()
+        }
         .onAppear {
-            var newList: [String] = []
-            sentiments.fetchUserFavorites { (tracks, error) in
-                if let error = error {
-                    print("Error fetching top tracks: \(error)")
-                } else if let tracks = tracks {
-                    for record in tracks {
-                        if let currentTrack = record["currentTrack"] as? String {
-                            newList.append(currentTrack)
-                        }
-                    }
-                    listItems = newList
-                }
-            }
+            loadData()
         }
     }
 }
