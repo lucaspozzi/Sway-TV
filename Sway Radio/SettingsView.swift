80b5c3d03d24805780393488d9530a2cfda75a7f
luc <lucaspozzi1@outlook.com>
2023-08-29 11:27:10 -0400

    LP: add app store review

--- Sway Radio/SettingsView.swift
+++ Sway Radio/SettingsView.swift
@@ -12,6 +12,25 @@ struct SettingsView: View {
     
     @State private var streamingQuality = UserDefaults.standard.integer(forKey: "useLowQuality")
     @EnvironmentObject var audioPlayer: AudioPlayer
+    @State var isReviewEnabled: Bool = true
+    
+    func getCurrentVersion() -> String {
+        // Get the current bundle version for the app
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+        return appVersion
+    }
+    
+    func checkReviewedVersions() {
+        
+        let currentVersion = getCurrentVersion()
+        
+        // Get last prompted version
+        let lastVersionPromptedForReview = UserDefaults.standard.string(forKey: "lastVersionPromptedForReviewKey")
+        
+        if(currentVersion == lastVersionPromptedForReview){
+            isReviewEnabled = false
+        }
+    }
     
     var body: some View {
         Form {
@@ -24,7 +43,20 @@ struct SettingsView: View {
             }
             
             Section(header: Text("Share"), footer: Text("We appreciate it!")) {
-                ShareLink("Send Sway to a friend", item: "https://apps.apple.com/us/app/sway-music-radio/id6451124668", message: Text("You will love Sway Music Radio, Viva los DJs!"))
+                ShareLink("Send Sway to a friend", item: "https://apps.apple.com/us/app/sway-music-radio/id6451124668", message: Text("You will love Sway Music Radio. Viva los DJs!"))
+            }
+            
+            Section(header: Text("Review"), footer: Text("Your feedback makes the app better and helps other listeners find Sway in the App Store.")) {
+                Button(action:{
+                    DispatchQueue.main.async {
+                        if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                            SKStoreReviewController.requestReview(in: scene)
+                        }
+                    }
+                    UserDefaults.standard.set(getCurrentVersion(), forKey: "lastVersionPromptedForReviewKey")
+                }){
+                    Text("Review in the App Store")
+                }.disabled(!isReviewEnabled)
             }
             
         }
@@ -32,6 +64,7 @@ struct SettingsView: View {
             UserDefaults.standard.set(newValue, forKey: "useLowQuality")
             audioPlayer.handleStreamingQualityChange()
         }
+        .onAppear(perform: checkReviewedVersions)
     }
 }
 
